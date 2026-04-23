6f67ff568906a5d7839bff59faaf36419cc6cac5
LAPTOP-90O3I2F7\yulik <yulikeren3@gmail.com>
2026-04-23 14:01:58 +0300

    fixed the vite option

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,5 +1,12 @@
-/// <reference types="vite/client" 
+/// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
 declare module '*.module.css' {
   const classes: { [key: string]: string };
   export default classes;
